5b3e2163de99c783f2c8c8a51ad5cff302e3ec9b
Jack Cooper <jc3123@pm.me>
2025-06-02 10:56:49 +0000

    updated image on home page to portrait

--- src/tests/test_home.py
+++ src/tests/test_home.py
@@ -4,4 +4,4 @@ def test_home(client):
     response = client.get('/')
     assert b"LinkedIn" in response.data
     assert b"github" in response.data
-    assert b"merc.png" in response.data
+    assert b"JackCooperPortrait.jpg" in response.data
