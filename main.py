9ea0140c949c783f887d0e8e0d93a9191a03c6a4
Jack Cooper <jc3123@pm.me>
2025-06-03 08:16:57 +0000

    added path manipulation to main.py to troubleshoot deployment issues

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+src_path = os.path.join(os.path.dirname(__file__), "src")
+if src_path not in sys.path:
+    sys.path.insert(0, src_path)
+
 from jcflask import create_app
 
 app = create_app()
